454725203868b1dfddaf6dfce1d63038196dbd08
Jimmy Ha <herereadthis@gmail.com>
2014-08-25 01:27:16 -0400

    tutorial through step 4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,66 @@
 'use strict';
 
 /* Controllers */
+/*
+A controller is a constructor function that takes a $scope parameter
+*/
+var phonecatApp = angular.module('phonecatApp', []);
+/*
+The data model is an array of phones as an object literal, instantiated within
+the PhoneListCtrl controller
+
+We've declared a controller called PhoneListCtrl and registered it as an 
+AngularJS module, phonecatApp
+
+The controller allows us to to establish data-binding between the model and the
+view.
+
+The ngController directive <body ng-controller="PhoneListCtrl"> references the
+name of the controller PhoneListCtrl
+
+The PhoneListCtrl controller attaches phone data to the $scope that was injected
+into the controller function. The $scope is a decendent of the root scope <html>
+This controller scope is available to all bindings within <body>
+*/
+
+/*
+$http service allows to make a HTTP request to web server, declared as a dependency
+manageed by DI (dependency injection) subsystem
+returns a promise object with a success method
+*/
+// must name the dependencies to allow for minification
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+        // $scope.phones = data;
+        // pre-process the result by limiting
+        $scope.phones = data.splice(0, 5);
+    });
+    $scope.orderProp = 'age';
+    $scope.name = "World";
+}]);
+
+// phonecatApp.controller('PhoneListCtrl', function ($scope) {
+//     $scope.phones = [
+//         {
+//             'name': 'Nexus S',
+//             'snippet': 'Fast just got faster with Nexus S.',
+//             'age': 1
+//         },
+//         {
+//             'name': 'Motorola XOOM™ with Wi-Fi',
+//             'snippet': 'The Next, Next Generation tablet.',
+//             'age': 2
+//         },
+//         {
+//             'name': 'MOTOROLA XOOM™',
+//             'snippet': 'The Next, Next Generation tablet.',
+//             'age': 3
+//         }
+//     ];
+//     $scope.name = "World";
+//     // set the default value of orderProp by age. Otherwise, the filter would 
+//     // remain uninitialized until the user selects an option.
+//     $scope.orderProp = 'age';
+// });
+
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,11 +1,57 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function() {
+    describe('PhoneListCtrl', function(){
+        // from angular mocks
+        var scope, ctrl, $httpBackend;
+        // before each test , tell Angular to load the phonecatApp module
+        beforeEach(module('phonecatApp'));
 
-describe('controllers', function() {
+        // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+        // This allows us to inject a service but then attach it to a variable
+        // with the same name as the service in order to avoid a name conflict.
+        // use inject helper method to inject instances of $rootscope, $controller, 
+        // and $httpBackend into Jasmine's beforeEach function
+        // mock service allows to write tests without having to deal with native APIs
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            // tell $httpBackend service  to expect an incoming HTTP request
+            $httpBackend.expectGET('phones/phones.json').
+            respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-  it("should do something", function() {
+            // new scope for our controller called $rootScope
+            scope = $rootScope.$new();
+            ctrl = $controller('PhoneListCtrl', {$scope: scope});
+        }));
+        it('should create "phones" model with 2 phones fetched from xhr', function() {
+            expect(scope.phones).toBeUndefined();
+            $httpBackend.flush();
 
-  });
+            expect(scope.phones).toEqual([
+                    {name: 'Nexus S'},
+                    {name: 'Motorola DROID'}
+                ]
+            );
+        });
+        it('should set the default value of orderProp model', function() {
+            expect(scope.orderProp).toBe('age');
+        });
+        // from earlier hard-coded data
+        // beforeEach(inject(function($controller) {
+        //     scope = {};
+        //     // We use $controller to create an instance of the PhoneListCtrl
+        //     ctrl = $controller('PhoneListCtrl', {$scope:scope});
+        // }));
+        // // We ask Angular to inject the $controller service into our test function
+        // it('should create "phones" model with 3 phones', function() {
 
+        //     expect(scope.phones.length).toBe(3);
+        //     expect(scope.name).toBe('World');
+        // });
+
+        // it('should set the default value of orderProp model', function() {
+        //     expect(scope.orderProp).toBe('age');
+        // });
+    });
 });
